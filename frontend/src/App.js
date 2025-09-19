be3d557bd172ffbab092df5e98ef9316aee84bd4
Tharun varshanth Krishnamoorthy <49119576+Tharunvarshanth@users.noreply.github.com>
2025-09-19 15:45:33 +0530

    Add React frontend boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Scrabble Game</h1>
+      <p>Welcome to your Scrabble board!</p>
+    </div>
+  );
+}
+
+export default App;
